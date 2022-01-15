39b21dcc6e4e712a46719b1f075f9f3c1d4f2ee2
Your Name <panchamiravi>
2022-01-15 15:11:11 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<?php
+	
+		$age=$array("peter"=>"35","ben"=>"37","joe"=>"43");
+		foreach($age as $x=>$val)
+		{
+			echo"$x=$val<br>";
+		}
+	?>
+
+</body>
+</html>
